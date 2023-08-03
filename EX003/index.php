a33f3dd7000c03da35ecef7b8879c8b89b99c499
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-02 21:16:36 -0300

    Create index.php

---
+++ EX003/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldicao</title>
+</head>
+<body>
+    
+        <?php 
+
+//Declaracao de variavel
+       $nome = "Guilherme";
+       $sobreNome = "Pinheiro";
+
+//Declaracao de conrtante
+       const PAIS = "Brasil";
+
+
+//Alteracao de variavel
+       $nome = "Amanda";
+       $sobreNome = "Marques";
+
+
+       echo "Muito Prazer, $nome $sobreNome! Voce mora no " . PAIS;
+        ?>
+
+        
+    
+    
+</body>
+</html>
