1aefc0af0ce9aa6081792a8e45c64d16bb6436be
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-03 18:21:39 -0300

    Create index.php

---
+++ EX004/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldicao</title>
+</head>
+<body>
+  <h1>Teste de tipos primitivos</h1>
+
+  <?php 
+  $num = 0;
+  echo "O valor da variavel e $num ";
+  ?>
+    
+</body>
+</html>
