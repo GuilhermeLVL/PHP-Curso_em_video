3eafeab6b92a4cfa004627cddfbc7efa164228aa
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2023-08-04 18:51:54 -0300

    Create index.php

---
+++ EX005/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldicao</title>
+</head>
+<body>
+  <h1>Teste de tipos primitivos</h1>
+
+  <?php 
+  $nome = "Guilherme";
+  $sobrenome = "Pinheiro";
+  $apelido = "Gui";
+  $ano = date('Y');
+
+  echo "Meu nome e $nome $sobrenome e meu apelido e \"$apelido\". 
+  <br> Estamos no ano de $ano"
+  ?>
+
+</body>
+</html>
